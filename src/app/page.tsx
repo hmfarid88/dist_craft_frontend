4ecfb4d175163155fde0dfe7e2381ee164c0d205
hmfarid88 <hmfariduddin189@gmail.com>
2025-04-12 16:20:00 +0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center h-screen p-5">
                 <div className="flex items-center justify-center bg-base-100">
                     <div>
-                        <FcHeatMap size={50} className='bnt btn-ghost rounded-md border border-cyan-500' /><span className='text-3xl font-extrabold font-sans'>BILLING</span> <span className='text-3xl font-extrabold text-accent font-sans'>CRAFT</span>
+                        <FcHeatMap size={50} className='bnt btn-ghost rounded-md border border-cyan-500' /><span className='text-3xl font-extrabold font-sans'>DIST</span> <span className='text-3xl font-extrabold text-accent font-sans'>CRAFT</span>
                         <p className='text-xs tracking-widest font-bold text-pretty'>SIMPLIFY YOUR BUSINESS</p>
                     </div>
                 </div>
