192ece70163a162d7108dbef7e186eae9292b10b
hmfarid88 <hmfariduddin189@gmail.com>
2026-04-03 00:59:13 +0600

    feature updated

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -30,7 +30,7 @@ export async function createSession(username: string, roles: string) {
 
   cookies().set('distcraft_session', session, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     expires: expiresAt,
     sameSite: 'lax',
     path: '/',
@@ -49,7 +49,7 @@ export async function updateSession() {
   const expires = new Date(Date.now() + 12 * 60 * 60 * 1000)
   cookies().set('distcraft_session', session, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     expires: expires,
     sameSite: 'lax',
     path: '/',
