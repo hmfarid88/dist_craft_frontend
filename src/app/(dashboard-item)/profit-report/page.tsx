dc6b8769e09cee0418dd1d61a95570fa3ac368dd
hmfarid88 <hmfariduddin189@gmail.com>
2026-06-03 19:55:15 +0600

    updated

--- src/app/(dashboard-item)/profit-report/page.tsx
+++ src/app/(dashboard-item)/profit-report/page.tsx
@@ -200,10 +200,9 @@ const Page = () => {
                         <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
                     </svg>
                 </label>
-                 <div className="flex">
-                <a href="#only_profit" className="btn btn-primary">Only Profit</a>
-            </div>
+                
                 <div className="flex gap-3">
+                    <a href="#only_profit" className="btn btn-primary btn-sm mt-2">Only Profit</a>
                     <ExcelExportButton tableRef={contentToPrint} fileName="RPDP_Report" />
                     <button onClick={handlePrint} className='btn btn-ghost btn-square'><FcPrint size={36} /></button>
                 </div>
